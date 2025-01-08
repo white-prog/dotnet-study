1fa4dbef496473d4674914b4f5dc07a38e26736c
white-prog <sabinhameed55@gmail.com>
2025-01-09 00:00:04 +0530

    Starting stage

---
+++ dotnet.py
@@ -0,0 +1,7 @@
+class Dotnet:
+    def __init__(self):
+        self.description = [".NET SDK is a tool used to create CLI, Desktop, Game and Web applications", "C# , F# , VB as Programming Language and SQL as database to create"]
+        self.basic_command_set = {"Create app : $$$$$$"}
+    def get_Command(self):
+        for i in self.basic_command_set:
+            print(i)
